6f1ccb1a28858785cba8c42f427729adcee55c4c
ishackm <45857636+ishackm@users.noreply.github.com>
2019-02-01 19:00:56 +0000

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, DATA
+
+app = Flask(__name__)
+
+photos = UploadSet('photos', DATA)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/csv'
+configure_uploads(app, photos)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        return filename
+    return render_template('upload.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+nbb
